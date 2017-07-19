3f2292649bf49c6eab91f4670397f4c5a9a0d96d
Abby Cohen <abbycohen@Susans-MacBook-Pro.local>
2017-07-19 19:31:49 -0400

    background color

--- src/app.jsx
+++ src/app.jsx
@@ -12,14 +12,72 @@ class App extends Component {
     this.state = {
       counter: 0,
       unusedStateVariable: 'bleeeeb',
+      switch: true,
+      x: 1,
+      value: 'Example text',
+      tempvalue: 'white',
+      usercolor: 'white'
     };
   }
-
 
   alertSomething = () => {
     alert('Clicked the basic handler, eh?');
   }
 
+
+  disco = () => {
+   while(this.state.x<5){
+     if(this.state.x===1){
+        document.body.style.backgroundColor='blue'
+        this.x.setState(2)
+      }
+     else if(this.state.x===2){
+        document.body.style.backgroundColor='red'
+        this.x.setState(3)
+      }
+     else if(this.state.x===3){
+        document.body.style.backgroundColor='green'
+        this.x.setState(4)
+      }
+     else{
+        document.body.style.backgroundColor='yellow'
+        this.x.setState(0)
+      }
+   }
+  }
+
+  submit = (evt) => {
+    console.log(evt)
+    this.setState({
+      tempvalue: evt.target.value
+    })
+  }
+
+  changeColor =(val) => {
+    console.log('changing')
+    this.setState({
+      value: this.state.tempvalue
+    })
+    document.body.style.backgroundColor=this.state.tempvalue
+  }
+
+  alertSpecial = () => {
+    alert('hiyah');
+  }
+
+  alertSwitch = () => {
+      if(this.state.switch===true){
+        document.body.style.backgroundColor='white'
+        alert('On')
+      }
+      else{
+        document.body.style.backgroundColor='black'
+        alert('Off')
+      };
+      this.setState({
+        switch: !this.state.switch
+      });
+  }
 
   addOne = () => {
     const newCounterValue = this.state.counter + 1;
@@ -47,6 +105,25 @@ class App extends Component {
           </button>
         </div>
 
+        <div className="special">
+          <button onClick={this.alertSpecial}>
+            ~special button~
+          </button>
+        </div>
+
+        <div>
+          <button onClick={this.alertSwitch}>
+            Light Switch
+          </button>
+
+        </div>
+
+        <div>
+          <button onClick={this.disco}>
+            Disco
+          </button>
+        </div>
+
         <div>
           <button onClick={this.addOne}>
             Add 1 To The Number
@@ -54,7 +131,11 @@ class App extends Component {
 
           <span> {this.state.counter} </span>
         </div>
-
+        <div>
+          <input type="text" value= {this.state.tempvalue} onChange={this.submit}/>
+          <button onClick={this.changeColor}> Change Color </button>
+        </div>
+          <div> {this.state.value} </div>
       </div>
     );
   }
