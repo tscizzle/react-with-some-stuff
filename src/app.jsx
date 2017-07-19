81679eb532a3b849e91cb75fcadfcf14c6bb851d
Kara Siegel <c19ks@dalton.org>
2017-07-19 19:31:09 -0400

    practice completed

--- src/app.jsx
+++ src/app.jsx
@@ -12,6 +12,10 @@ class App extends Component {
     this.state = {
       counter: 0,
       unusedStateVariable: 'bleeeeb',
+      randomNumber: 1,
+      writing: "On",
+      value: "",
+      tempValue: "",
     };
   }
 
@@ -29,17 +33,52 @@ class App extends Component {
     });
   }
 
+  changeNumber = () => {
+    const newNumber =  Math.ceil(Math.random() * 20)
+
+    this.setState({
+      randomNumber: newNumber,
+    });
+  }
+
+  onOff = () => {
+    let newWord = ""
+    if (this.state.writing === "On"){
+      newWord = "Off"
+      this.setState({color: "black"});
+    }
+    else{
+      newWord = "On"
+      this.setState({color: "white"})
+    }
+
+    this.setState({
+      writing: newWord,
+    });
+  }
+
+  submit = (evt) => {
+    this.setState({tempValue: evt.target.value});
+  }
+
+  background = () => {
+    this.setState({color: this.state.tempValue});
+    this.setState({value: this.state.tempValue})
+  }
+
 
   render() {
+    var style = {backgroundColor: this.state.color};
     return (
-      <div className="app">
-
+      <div className="app" style={style}>
         <div className="app-header">
           <img src={logo}
                className="app-logo"
                alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+
+     
 
         <div>
           <button onClick={this.alertSomething}>
@@ -55,6 +94,27 @@ class App extends Component {
           <span> {this.state.counter} </span>
         </div>
 
+        <div>
+          <button onClick = {this.changeNumber}>
+            Click for a random number
+          </button>
+
+            <span> {this.state.randomNumber}</span>
+        </div>
+
+        <div>
+          <button onClick={this.onOff}>
+            Turn on / off
+          </button>
+
+            <span> {this.state.writing}</span>
+        </div>
+
+        <span>
+          <input type="text" onChange={this.submit}></input>
+          <button onClick={this.background}>Pick a color</button>
+          <div> {this.state.value}</div>
+        </span>
       </div>
     );
   }
